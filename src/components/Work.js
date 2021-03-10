42919229d67f8c877a5e310654c7779616337eb7
zxum <zion.um@outlook.com>
2021-03-10 12:54:14 -0600

    Remove and Add Work Experiences

---
+++ src/components/Work.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import contentEditable from './contentEditable'
+
+function Work(props) {
+
+  let EditableP = contentEditable('p')
+  let EditableSpan = contentEditable('span')
+  let EditableH3 = contentEditable('h3')
+
+  return (
+    <div className="work-info">
+      <div className="flex-wrapper right-side">
+        <i className="fas fa-times-circle btn" 
+          id="delete-btn"
+          onClick={()=>{props.deleteWork(props.data.id)}}></i>
+      </div>
+      <div className="flex-wrapper">
+        <div className="dates">
+            <EditableP value={props.data.from} /> to <EditableP value={props.data.to} />
+        </div>
+        <div className="other">
+          <EditableH3 value={props.data.position} /> 
+          <div className="italic">
+            <EditableSpan value={props.data.company} />, <EditableSpan value={props.data.location} />
+          </div>
+          <EditableP value={props.data.description} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Work
--- src/components/WorkExperiences.js
+++ src/components/WorkExperiences.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-
+import uniqid from 'uniqid'
+import Work from './Work'
 export class WorkExperiences extends Component {
 
   constructor() {
     super() 
     this.state = {
-      showAddBtn: true
+      showAddBtn: true,
       workexps: [],
       work: {
         id:uniqid(),
@@ -20,19 +21,16 @@ export class WorkExperiences extends Component {
     this.toggleAddBtn = this.toggleAddBtn.bind(this)
     this.handleCancel = this.handleCancel.bind(this)
     this.handleDone = this.handleCancel.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  toggleAddBtn() {
+  toggleAddBtn = () => {
     this.setState({
       showAddBtn: !this.state.showAddBtn
     })
   }
 
-  handleDone() {
-    
-  }
-
-  handleCancel(event) {
+  handleCancel = (event) => {
     let form = event.target.parentNode.parentNode
     console.log(form)
     
@@ -40,21 +38,79 @@ export class WorkExperiences extends Component {
     this.toggleAddBtn()
   }
 
-  save() {
+  handleChange = (event) => {
+    let {name, value} = event.target
 
+    this.setState({
+      work: {
+        ...this.state.work,
+        [name]: value
+      }
+    })
+  }
+
+  handleDone = () => {
+    this.save()
+    this.toggleAddBtn()
+    this.reset()
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault() 
+
+    this.save()
+    this.toggleAddBtn()
+    this.reset()
+  }
+
+  deleteWork = (id) => {
+    let newWorkList = this.state.workexps.filter(work => {
+      return work.id != id
+    })
+    this.setState({
+      workexps: newWorkList
+    })
+  }
+
+  save = () => {
+    this.setState({
+      workexps:[
+        ...this.state.workexps, 
+        this.state.work]
+    })
+  }
+
+  reset = () => {
+    this.setState({
+      work: {
+        ...this.state.work,
+        id: uniqid(),
+        from: '',
+        to: '',
+        company: '',
+        location: '',
+        position: '',
+        description: ''
+      }
+    })
   }
 
 
   render() {
+    let workList = this.state.workexps.map((work)=>{
+      return <Work key={work.id} data={work} deleteWork={this.deleteWork} />
+    })
     if (this.state.showAddBtn) {
       return (
         <div className="work-block">
+          {workList}
           <i className="fas fa-plus-circle" id="add-btn" onClick={this.toggleAddBtn}></i>
         </div>
       )
     } else {
       return (
       <div className="work-block">
+        {workList}
         <form className="work-input">
           <div className="flex-wrapper right-side">
             <i className="fas fa-check-circle btn" id="done-btn" onClick={this.handleSubmit}></i>
@@ -63,28 +119,28 @@ export class WorkExperiences extends Component {
 
           <div className="flex-wrapper">
             <label htmlFor="from">From: 
-              <input type="text" id="from" placeholder="YYYY"/>
+              <input type="text" name="from" placeholder="YYYY" onChange={this.handleChange}/>
             </label>
             <label htmlFor="to">To: 
-              <input type="text" id="to" placeholder="YYYY" />
+              <input type="text" name="to" placeholder="YYYY" onChange={this.handleChange} />
             </label>
           </div>
 
           <div className="flex-wrapper">
             <label htmlFor="company">Company: 
-              <input type="text" id="company" />
+              <input type="text" name="company" onChange={this.handleChange} />
             </label>
             <label htmlFor="location">Location: 
-              <input type="text" id="location" />
+              <input type="text" name="location" onChange={this.handleChange} />
             </label>
           </div>
           
           <label htmlFor="position">Position: 
-            <input type="text" id="position" />
+            <input type="text" name="position" onChange={this.handleChange} />
           </label>
         
           <label htmlFor="description">Description: 
-            <textarea name="" id="description"></textarea>
+            <textarea name="description" onChange={this.handleChange}/>
           </label>
 
         </form>
